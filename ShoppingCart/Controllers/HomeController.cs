b049d90c517d0c2f249ea6f861e628790565588b
MehboobAzeem <30482100+MehboobAzeem@users.noreply.github.com>
2017-09-10 08:39:04 -0700

    commit

---
+++ ShoppingCart/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoppingCart.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [Route("api/GetProducts")]
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            var db = new ShoopingCartEntities();
+            db.Configuration.ProxyCreationEnabled = false;
+
+            return db.Products.ToList();
+        }
+    }
+}
